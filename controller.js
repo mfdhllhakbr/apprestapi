23fa30847a1b63bc13c66bb7d237b26d0dce4cc3
Fadhillah Akbar <fadhillahakbar@Adi-MacBook.local>
2023-05-04 23:47:33 +0700

    berhasil hapus data mahasiswa

--- controller.js
+++ controller.js
@@ -64,3 +64,16 @@ exports.ubahMahasiswa = function(req, res){
         }
     });
 };
+
+//menghapus data berdasarkan id mahasiswa
+exports.hapusMahasiswa = function(req, res){
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error)
+        } else {
+            response.ok("Berhasil Menghapus Data", res)
+        }
+    });
+};
