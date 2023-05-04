bdf40902a730b98457bc2b6038f514ccbaa6ecc0
Fadhillah Akbar <fadhillahakbar@Adi-MacBook.local>
2023-05-05 02:20:13 +0700

    install package morgan, ip, tokenjwt

--- res.js
+++ res.js
@@ -9,3 +9,33 @@ exports.ok = function(values, res){
     res.json(data);
     res.end();
 }
+
+//response untuk nested matakuliah
+exports.oknested = function(values, res){
+    //melakukan akumulasi
+    const hasil = values.reduce((akumulasikan, item)=>{
+        //tentukan keygroupnya
+        if(akumulasikan[item.nama]){
+            //buat variabel group nama mahasiswa
+            const group = akumulasikan[item.nama];
+            //cek jika isi array adalah mata kuliah
+            if(Array.isArray(group.matakuliah)){
+                //tambah value ke dalam group matakuliah
+                group.matakuliah.push(item.matakuliah)
+            } else{
+                group.matakuliah = [group.matakuliah, item.matakuliah]
+            }
+        } else{
+            akumulasikan[item.nama] = item
+        }
+        return akumulasikan;
+    }, {});
+
+    var data = {
+        'status':200,
+        'values':hasil
+    }
+
+    res.json(data);
+    res.end();
+}
